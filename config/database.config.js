2d25405601b7dea22eef8e70ac36b2cc0ba95e0a
dewa qintoro <dewaqintoro@gmail.com>
2020-05-06 01:56:56 +0700

    v2

--- config/database.config.js
+++ config/database.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    url: 'mongodb://localhost:27017/easy-notes'
+    url: 'mongodb://localhost/easy-notes'
     // url: 'mongodb+srv://dewa:dewa@clusterrestfullapi-1imbk.azure.mongodb.net/test?retryWrites=true&w=majority'
 }
 
